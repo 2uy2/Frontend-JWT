74cec6d1a9a242629043d0b2203c28ed2619636a
tran dinh ngoc quy <quytran11204@gmail.com>
2026-04-28 14:05:20 +0700

    css login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,22 +2,26 @@ import "./Login.scss"
 
 const Login=(props)=>{
     return (
-        <div className="login-container mt-3">
+        <div className="login-container my-auto">
             <div className="container">
-                <div className="row">
-                    <div className="content-left col-7">
-                        <div className="brand">Quy</div>
+                <div className="row px-3 px-sm-0">
+                    <div className="content-left col-12 d-none col-sm d-sm-block">
+                        <div className="brand">Quy dep trai</div>
                         <div className="detail">learn everything </div>
                     </div>
-                    <div className="content-right col-5 d-flex flex-column gap-3 py-3">
+                    
+                    <div className="content-right col-sm-5 col-12 d-flex flex-column gap-3 py-3 ">
+                        <div className="brand d-sm-none">Quy dep trai</div>
                         <input className="form-control" type="text" placeholder="email address or phone number"/>
                         <input className="form-control" type="password" placeholder="password"/>
                         <butto className="btn btn-primary">login</butto>
-                        <div className="text-center">forgot your password</div>
+                        <div className="text-center"><a className="forgot-password" href="#">forgot your password</a></div>
                         <hr/>
                         <div className="text-center">
                             <button className="btn btn-success">Create new account ?</button>
-                        </div>
+                    </div>
+                   
+                    
                         
 
                     </div>
