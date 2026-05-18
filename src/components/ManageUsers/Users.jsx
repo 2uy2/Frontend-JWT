bf33f2a41af116d75502f9722da44625d32319c6
tran dinh ngoc quy <quytran11204@gmail.com>
2026-05-18 09:34:53 +0700

    pagipnation

--- src/components/ManageUsers/Users.jsx
+++ src/components/ManageUsers/Users.jsx
@@ -1,73 +1,104 @@
 import { useEffect, useState } from "react"
 import { fetchAllUser } from "../../services/userService";
-import './Users.scss'
+import './Users.scss';
+import ReactPaginate from 'react-paginate';
 const Users =(props)=>{
     const [listUsers,setListUsers] = useState([]);
+    const [currentPage,setCurrentPage]=useState(1);
+    const [currentLimit,setCurrentLimit]=useState(5);
+    const [totalPages,setTotalPages]= useState(0);
     useEffect(()=>{
         fetchUsers();
 
-    },[])
+    },[currentPage])
    
-    const fetchUsers = async ()=>{
-        let response = await fetchAllUser();
+    const fetchUsers = async (page)=>{
+        let response = await fetchAllUser(page?page:currentPage,currentLimit);
         if(response && response.data && response.data.EC ===0){
-            setListUsers(response.data.DT);
-            console.log(response.data.DT)
+            // setListUsers(response.data.DT);
+            console.log(response.data.DT);
+            setTotalPages(response.data.DT.totalPages);
+            setListUsers(response.data.DT.users)
+
         }
     }
+    const handlePageClick = async(event) => {
+        setCurrentPage(+event.selected+1);
+      
+    };
     return  (
         <div className="container">
             <div className="manage-user-container">
-            <div className="user-header">
-                    <div className="title">
-                        <h3>Table users</h3>
-                    </div>
-                    <div className="actions">
-                        <button className="btn btn-success">Refresh</button>
-                        <button className="btn btn-primary">Add new user</button>
-                    </div>
-            </div>
-            <div className="user-body">
-                    <table className="table table-hover table-bordered">
-                        <thead>
-                            <tr>
-                            <th scope="col">No</th>
-                            <th scope="col">Id</th>
-                            <th scope="col">Email</th>
-                            <th scope="col">Username</th>
-                            <th scope="col">Group </th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {listUsers && listUsers.length >0 ? <>
-                                {listUsers.map((item,index)=>{
-                                    return (
-                                        <tr key={`row-${index}`}>
-                                            <td>{index+1}</td>
-                                            <td>{item.id}</td>
-                                            <td>{item.email}</td>
-                                            <td>{item.username}</td>
-                                            <td>{item.Group ? item.Group.name :''}</td>
-                                        </tr>
-                                    )
-                                })}
-                            </>:
-                            <span>not found users</span>}
-                            
-                        </tbody>
-                    </table>
-            </div>
-            <div className="user-footer">
-                    <nav aria-label="Page navigation example">
-                        <ul class="pagination">
-                            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
-                            <li class="page-item"><a class="page-link" href="#">1</a></li>
-                            <li class="page-item"><a class="page-link" href="#">2</a></li>
-                            <li class="page-item"><a class="page-link" href="#">3</a></li>
-                            <li class="page-item"><a class="page-link" href="#">Next</a></li>
-                        </ul>
-                        </nav>
-            </div>
+                ``<div className="user-header">
+                        <div className="title">
+                            <h3>Table users</h3>
+                        </div>
+                        <div className="actions">
+                            <button className="btn btn-success">Refresh</button>
+                            <button className="btn btn-primary">Add new user</button>
+                        </div>
+                </div>
+                <div className="user-body">
+                        <table className="table table-hover table-bordered">
+                            <thead>
+                                <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Id</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Username</th>
+                                <th scope="col">Group </th>
+                                <th scope="col">Actions </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {listUsers && listUsers.length >0 ? <>
+                                    {listUsers.map((item,index)=>{
+                                        return (
+                                            <tr key={`row-${index}`}>
+                                                <td>{index+1}</td>
+                                                <td>{item.id}</td>
+                                                <td>{item.email}</td>
+                                                <td>{item.username}</td>
+                                                <td>{item.Group ? item.Group.name :''}</td>
+                                                <td>
+                                                    
+                                                    <button className="btn btn-warning">Edit</button>
+                                                    <button className="btn btn-danger">Delete</button>
+                                                </td>
+                                            </tr>
+                                        )
+                                    })}
+                                </>:
+                                <tr><td>not found users</td></tr>}
+                                
+                            </tbody>
+                        </table>
+                </div>
+                {totalPages >0 && 
+                <div className="user-footer">
+                    <ReactPaginate
+                        nextLabel="next >"
+                        onPageChange={handlePageClick}
+                        pageRangeDisplayed={3}
+                        marginPagesDisplayed={2}
+                        pageCount={totalPages}
+                        previousLabel="< previous"
+                        pageClassName="page-item"
+                        pageLinkClassName="page-link"
+                        previousClassName="page-item"
+                        previousLinkClassName="page-link"
+                        nextClassName="page-item"
+                        nextLinkClassName="page-link"
+                        breakLabel="..."
+                        breakClassName="page-item"
+                        breakLinkClassName="page-link"
+                        containerClassName="pagination"
+                        activeClassName="active"
+                        renderOnZeroPageCount={null}
+                    />
+                </div>
+                }
+                
             </div>
         </div>
         
